8b72173b4141285ccb74c6a7a9e28c31f06eb32c
Anthony Truskinger <anthony.truskinger@gmail.com>
2015-01-30 14:23:25 +1000

    Added UMD & bower support, bumped version

---
+++ roundDate.js
@@ -0,0 +1,53 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
+}(this, function () {
+
+    function roundDate(roundStyle, roundToSeconds, date) {
+        if (arguments.length !== 3) {
+            throw new Error("Expected  2 arguments");
+        }
+
+        var startOfDay = new Date(date).setHours(0, 0, 0),
+            roundToMilliseconds = roundToSeconds * 1000,
+            msFromMidnight = (+date) - startOfDay,
+            remainder = msFromMidnight % roundToMilliseconds,
+            result = msFromMidnight;
+
+        if (remainder !== 0) {
+            if (roundStyle === "round") {
+                var fraction = (msFromMidnight / roundToMilliseconds) % 1;
+                roundStyle = fraction >= 0.5 ? "ceil" : "floor";
+            }
+
+            if (roundStyle === "floor") {
+                result -= remainder
+            }
+            else if (roundStyle === "ceil") {
+                result += (roundToMilliseconds - remainder);
+            }
+            else {
+                return;
+            }
+        }
+
+        return new Date(startOfDay + result);
+    }
+
+    return {
+        round: roundDate.bind(null, "round"),
+        floor: roundDate.bind(null, "floor"),
+        ceil: roundDate.bind(null, "ceil")
+    };
+}));
+
