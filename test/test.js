1aaf487d5099d9090b6faee24aeb72faa85bd3a1
Anthony Truskinger <anthony.truskinger@gmail.com>
2015-03-30 19:10:41 +1000

    Fixes #1, milliseconds not correctly rounded.
    Also cleaned up unit tests, fixed broken test state, and added new tests for millisecond rounding functionality.

--- test/test.js
+++ test/test.js
@@ -1,55 +1,72 @@
 var test = require('tape'),
-    roundDate = require("../index.js");
+    roundDate = require("../roundDate.js");
+
+var now = new Date(),
+    nowTestName = "millisecond rounding test, interval=60min, for random date:" + now.toISOString();
 
 var testCases = {
-    "basic test, interval=60": {
+    "basic test, interval=60min": {
         date: "Fri Jan 30 2015 09:33:49 GMT+1000",
         round: "Fri Jan 30 2015 10:00:00 GMT+1000",
         floor: "Fri Jan 30 2015 09:00:00 GMT+1000",
         ceil: "Fri Jan 30 2015 10:00:00 GMT+1000",
         interval: 60 * 60
     },
-    "rounding down, interval=60": {
+    "rounding down, interval=60min": {
         date: "Fri Jan 30 2015 09:15:00 GMT+1000",
         round: "Fri Jan 30 2015 09:00:00 GMT+1000",
         floor: "Fri Jan 30 2015 09:00:00 GMT+1000",
         ceil: "Fri Jan 30 2015 10:00:00 GMT+1000",
         interval: 60 * 60
     },
-    "no rounding, interval=60": {
+    "no rounding, interval=60min": {
         date: "Fri Jan 30 2015 09:00:00 GMT+1000",
         round: "Fri Jan 30 2015 09:00:00 GMT+1000",
         floor: "Fri Jan 30 2015 09:00:00 GMT+1000",
         ceil: "Fri Jan 30 2015 09:00:00 GMT+1000",
         interval: 60 * 60
     },
-    "basic test, interval=45": {
+    "basic test, interval=45min": {
         date: "Fri Jan 30 2015 09:15:00 GMT+1000",
         round: "Fri Jan 30 2015 09:00:00 GMT+1000",
         floor: "Fri Jan 30 2015 09:00:00 GMT+1000",
         ceil: "Fri Jan 30 2015 09:45:00 GMT+1000",
         interval: 45 * 60
     },
-    "basic test, interval=5": {
+    "basic test, interval=5min": {
         date: "Fri Jan 30 2015 09:16:00 GMT+1000",
         round: "Fri Jan 30 2015 09:15:00 GMT+1000",
         floor: "Fri Jan 30 2015 09:15:00 GMT+1000",
         ceil: "Fri Jan 30 2015 09:20:00 GMT+1000",
         interval: 5 * 60
     },
-    "basic test, interval=90": {
+    "basic test, interval=90min": {
         date: "Fri Jan 30 2015 17:15:00 GMT+1000",
         round: "Fri Jan 30 2015 18:00:00 GMT+1000",
         floor: "Fri Jan 30 2015 16:30:00 GMT+1000",
         ceil: "Fri Jan 30 2015 18:00:00 GMT+1000",
         interval: 90 * 60
     },
-    "day overflow test, interval=70": {
+    "day overflow test, interval=70min": {
         date: "Fri Jan 30 2015 23:30:00 GMT+1000",
         round: "Fri Jan 30 2015 23:20:00 GMT+1000",
         floor: "Fri Jan 30 2015 23:20:00 GMT+1000",
         ceil: "Sat Jan 31 2015 00:30:00 GMT+1000",
         interval: 70 * 60
+    },
+    nowTestName: {
+        date: now,
+        round: new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + Math.round(now.getMinutes() / 60)),
+        floor: new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours()),
+        ceil: new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1),
+        interval: 60 * 60
+    },
+    "millisecond rounding test, interval=0.5 seconds": {
+        date: "Fri Jan 30 2015 09:33:49.123 GMT+1000",
+        round: "Fri Jan 30 2015 09:33:49.000 GMT+1000",
+        floor: "Fri Jan 30 2015 09:33:49.000 GMT+1000",
+        ceil: "Fri Jan 30 2015 09:33:49.500 GMT+1000",
+        interval: 0.5
     }
 };
 
@@ -71,13 +88,13 @@ Object.keys(testCases).map(function(key) {
     test(key, function (t) {
         var testCase = testCases[key],
             d = new Date(testCase.date),
-            a = roundDate.round(testCase.interval, d),
-            b = roundDate.floor(testCase.interval, d),
-            c = roundDate.ceil(testCase.interval,  d);
+            rounded = roundDate.round(testCase.interval, d),
+            floored = roundDate.floor(testCase.interval, d),
+            ceilinged = roundDate.ceil(testCase.interval,  d);
 
-        t.equal(a.toISOString(), (new Date(testCase.round)).toISOString(), "round operation");
-        t.equal(b.toISOString(), (new Date(testCase.floor)).toISOString(), "floor operation");
-        t.equal(c.toISOString(), (new Date(testCase.ceil )).toISOString(), "ceil  operation");
+        t.equal(rounded.toISOString(), (new Date(testCase.round)).toISOString(), "round operation");
+        t.equal(floored.toISOString(), (new Date(testCase.floor)).toISOString(), "floor operation");
+        t.equal(ceilinged.toISOString(), (new Date(testCase.ceil )).toISOString(), "ceil  operation");
         t.end();
     });
 });
